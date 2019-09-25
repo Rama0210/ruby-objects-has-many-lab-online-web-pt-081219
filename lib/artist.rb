546c1def035e0697f9399ff2f0ac6fed924b4daf
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-25 12:01:30 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+   
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  
+  def add_song_by_name(song_name)
+    song = Song.new (song_name)
+add_song(song)
+end
+end
+
